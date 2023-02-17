f2cd5b348cab6cc19a0e4350784ab7ca9fb6b114
KialoHezz <hezzykialo.official.ke@gmail.com>
2023-02-17 11:44:10 +0300

    Build navigation

---
+++ js/index.js
@@ -0,0 +1,45 @@
+document.getElementById("btn").addEventListener("click", function(){
+    this.ontoggle
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded",
+//     function(event){
+//         function sayHello(event){
+//             this.textContent = "SAID IT";
+
+//             let name = document.getElementById("name").value;
+//             let message = "<h2>Hello " + name + "!</h2>";
+
+//             document.getElementById("message").innerHTML = message;
+
+
+//             if(name === "student"){
+//                 let title = document.querySelector("h5").textContent;
+//                 title = "Hi Student & Welcome";
+
+//                 document.querySelector("h5").textContent = title;
+//             }
+//         }
+
+//         document.querySelector("button").addEventListener("click", sayHello);
+
+//         document.querySelector("body").addEventListener("mousemove", function(event){
+//             if(event.shiftKey === true){
+//                 console.log("X : " + event.clientX)
+//                 console.log("X : " + event.clientY)
+//             }
+//         })
+//     }
+// );
+
